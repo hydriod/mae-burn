9ee3c998b2a65a78b9ca67c8c462de0fa5945b00
hydriod <bibi6548@gmail.com>
2026-07-12 00:08:10 +0900

    lib.rs: change module declarations to public and remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,5 @@
-mod model;
-mod layer;
+pub mod model;
+pub mod layer;
 
+pub use model::{MaskedAutoencoderViT, MaskedAutoencoderViTConfig};
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
